e29703ed5f2a64cdf553ecfd309c48f7ee4babef
Eric Dennison <ericdennison@ericdennison1415.local>
2015-02-20 17:39:25 -0500

    Fixed some Python3 stuff

--- test.py
+++ test.py
@@ -2,8 +2,9 @@
 import subprocess
 
 def test():
-  proc = subprocess.Popen(['python', 'hello.py',  ''], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-  res = proc.communicate()[0]
+  proc = subprocess.Popen(['python3', 'hello.py',  ''], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  res = proc.communicate()[0].decode("utf-8").strip()
+  print("Result:", res)
   if res != "Hello, world.":
     print("Test Fail!")
     assert False
