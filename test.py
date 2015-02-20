f0f6eeea32fbd67a2729a35eff2313c343b05033
Eric Dennison <ericd@netdenizen.com>
2015-02-20 17:27:59 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+# Script for automatic testing of hello.py
+import subprocess
+
+def test():
+  proc = subprocess.Popen(['python', 'hello.py',  ''], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  res = proc.communicate()[0]
+  if res != "Hello, world!":
+    assert False
