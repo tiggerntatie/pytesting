5946bc95f5b4c8240f6a91c3e6e5db8cc2fad3f8
Eric Dennison <ericd@netdenizen.com>
2015-02-20 17:32:24 -0500

    Human readable results!

--- test.py
+++ test.py
@@ -5,4 +5,7 @@ def test():
   proc = subprocess.Popen(['python', 'hello.py',  ''], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   res = proc.communicate()[0]
   if res != "Hello, world.":
+    print("Test Fail!")
     assert False
+  else:
+    print("Test Pass!")  
