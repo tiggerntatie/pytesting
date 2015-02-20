be716eddb9ace27bb5ceeee30e93a142ecba17d4
Eric Dennison <ericd@netdenizen.com>
2015-02-20 17:28:32 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ import subprocess
 def test():
   proc = subprocess.Popen(['python', 'hello.py',  ''], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   res = proc.communicate()[0]
-  if res != "Hello, world!":
+  if res != "Hello, world.":
     assert False
